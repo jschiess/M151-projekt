dbc20babbc2e3864d6e641d5ddc163554e2a48a0
Nico Sieber <nicosieber@gmx.ch>
2019-05-27 09:09:53 +0200

    .

--- app.js
+++ app.js
@@ -1,102 +1,76 @@
 const express = require('express')
-const knex = require('./knex/knex')
+const knex = require('./knex/knex.js');
 
+console.log('Willkomen zu unserem Quiz Backend!');
 
-console.log('Willkommen zu unserem Quiz-Backend!')
-
-var gugus = "Gugus"
-
-const app = express()
-
-app.use(express.json())
-
-app.get('/gugus', (req, res) => {
-  console.log('Jemand will Gugus von uns!')
-  //console.log(req)
-  if (req.query.speziell === 'true') {
-    res.json({ data: 'Spezieller ' + gugus })
-  } else {
-    res.json({ data: gugus })
-  }
+app.get('/api/quiz', async(req, res) => {
+    result = await knex('quiz')
+                    .select('*')
+    res.json(result)
+})
+app.get('/api/quiz/:id', async(req, res) => {
+    result = await knex('quiz')
+                    .select('*')
+                    .where('id', req.params.id)
+    if(result.length === 0){
+        res.status(404)
+        res.send('NOT FOUND!\n')
+        return
+    }else{
+        res.send(result[0])
+    }
 })
 
-
-app.get('/user', async (req, res) => {
-  var kek = await knex('user')
-  console.log('better than not! '+ kek)
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // res.send('ok')
-    res.json(kek)
-})
-
-
-app.put('/gugus', (req, res) => {
-  gugus = req.body.data
-  res.send('OK')
-})
-
-async function test() {
-  var test = await knex('quiz')
-  var result = await knex('userb ')
-  console.log(result);
-}
-
-// test()
-
-
-/**
- * Josiah's trashy code;
- * TODO: 
- * 
- * 
- */
-
-
-app.post('/quiz', async (req, res) => {
-  console.log(req.body.name);
-
-  await knex('quiz').insert([
-    { 'name': req.body.name, creator_id: 1 },
-  ])
-
-
-  res.send('OK')
-})
-
-
-
-
-
-//using url to pass variables
-
-app.delete('/quiz/:id', async (req, res) => {
-  var result = await knex('quiz').where('id', req.params.id)
-
-  if (result.length === 0) {
-    res.status(404)
-    res.send('quiz not found')
-  } else{
-    await knex('quiz').where({id: req.params.id}).del()
-    res.send('OK')
-
-  }
+app.get('/api/quiz/:id/questions', async(req, res) => {
+    let result = await knex('question')
+                        .select('answer.answer', 'answer.is_correct')
+                        .leftJoin('answer', 'question.id', 'answer.question_id')
+                        .where('question.quiz_id', req.params.id)
+                        .orderBy('question.order')
+ 
+    // Pro frage ein Objekt //Liste für anwtworten
+    /*   let sendResult = []
+    foreach(question.id in result){
+        sendResult.push({})
+    } */
+    res.send([result])
 
 })
 
+app.post('/api/quiz', async(req, res) => {
+    await knex('quiz').insert([
+        {name: req.body.name},
+    ]);
+    res.send('OK!')
+})
 
-// test()
+app.delete('/api/quiz/:id', async (req, res) => {
+    let result = await knex('quiz').where({id: req.params.id})
+    if(result.length === 0) {
+        res.status(404)
+        res.send('NOT FOUND\n')
+        return
+    }
+    result = await knex('quiz')
+                .where({id: req.params.id})
+                .del()
+    console.log(result)
+    res.send('OK\n')
+})
 
+app.put('/api/quiz/:id', async(req, res) => {
+    let result = await knex('quiz').where({id: req.params.id})
+    if(result.length === 0) {
+        res.status(404)
+        res.send('NOT FOUND\n')
+        return
+    }else{
+        let result = await knex('quiz')
+                            .where({id: req.params.id})
+                            .update({name: req.body.name})
+        console.log(result)
+        res.send('OK\n')
+    }   
+})
 
-// app.get('quiz', async (req,res) => {
-//   try {
-//     await knex('quiz')
-
-//   } catch (err) {
-//     console.log(err);
-//     res.send(err)
-//   }
-
-// })
-
-app.listen(80)
+app.listen(3000, () => console.log("Listening on port 3000"))
