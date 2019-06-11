5dd36a6b3426c737bbf47f5eb2f51a7bcdbd9367
=Holoarcher <josiah.schiess@gmail.com>
2019-06-12 01:15:54 +0200

    .

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const knex = require('./knex/knex.js');
 const app = express()
 
 var kek = -8
-var gameState = true
+var gameState = 1
+
+// gameStates
+//   1: game is not on :: kek is 0
+//   2: game is running :: kek is counting up
+//   3 game is paused :: kek is not counting
 
 console.log('Willkomen zu unserem Quiz Backend!');
 
@@ -35,6 +40,16 @@ app.get('/api/quiz/users', async (req, res) => {
     res.send(result)
 })
 
+
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+
+// Ich brauch auch active users die noch kein eintrag gemacht haben
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+
 // Get all active users
 app.get('/api/quiz/active_users', async (req, res) => {
 
@@ -45,18 +60,20 @@ app.get('/api/quiz/active_users', async (req, res) => {
         .join('answer', 'user_answer.answer_id', 'answer.id')
         .select('user.nick')
         .where('is_active', 1)
+        .where('is_correct', 1)
         .count('answer.is_correct as correct')
         .groupBy('user.nick')
-        .orderBy('correct', 'desc')
+        .select('user.id')
+        // .orderBy('correct', 'desc')
 
-
-    if (result.length === 0) {
-        res.status(404)
-        res.send('NOT FOUND!\n')
-        return
-    } else {
         res.send(result)
-    }
+
+    // if (result.length === 0) {
+    //     res.status(404)
+    //     res.send('NOT FOUND!\n')
+    // } else {
+    //     res.send(result)
+    // }
 
 })
 
@@ -101,7 +118,7 @@ app.get('/api/quiz/:id/questions', async (req, res) => {
         return
     } else {
 
-        console.log(questions);
+        // console.log(questions);
 
         for (question of questions) {
 
@@ -187,12 +204,18 @@ app.post('/api/quiz/user', async (req, res) => {
 })
 
 // Changes the atribute is_active from true to false
-app.put('/api/quiz/user/change_isactive', async (req, res) => {
+async function change_isactive() {
+    
     await knex('user').where({
         is_active: true
     }).update({
         is_active: false
     })
+    
+}
+// alle user zu inactive
+app.put('/api/quiz/user/change_isactive', async (req, res) => {
+    change_isactive()
     res.send('OK\n')
 })
 
@@ -263,54 +286,164 @@ app.delete('/api/quiz/:id', async (req, res) => {
     res.send('OK\n')
 })
 
-app.get('/api/kek', (req, res) => {
+
+
+
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+
+// 👏Josiah's👏 trashy👏 ductape 👏 code👏 💘💘💘💘💘 
+// there are no mistakes just happy accidents 
+// 🔫🔫🔫🔫🔫🔫🔫🔫🔫🔫
+
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+// 👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏#👏
+
+
+
+// also jsut for testin
+        app.get('/api/justdont', async (req, res) => {
+
+            let user = knex('user').select('*')
+
+            let result = await knex('user')
+                .leftJoin('user_answer', 'user.id', 'user_answer.user_id')
+                // .leftJoin('answer', 'user_answer.answer_id', 'answer.id')
+                // .select('user.nick')
+                .where('is_active', 1)
+                // .where('nick', 'aaaa')
+                .select('*')
+                
+                // .where('is_correct', 1)
+                // .count('answer.is_correct as correct')
+                // .groupBy('user.nick')
+                // .orderBy('correct', 'desc')
+                console.log(result);
+
+
+            if (result.length === 0) {
+                
+                res.status(404)
+                res.send('NOT FOUND!\n')
+            } else {
+                res.send(result)
+            }
+
+        })
+
+
+
+
+app.get('/api/kek', async (req, res) => {
 
     res.send({ kek: kek })
 })
 
+app.get('/api/users', async (req, res) => {
+    let result = await knex('user')
 
-
-// Josiah's trashy code
-
-
-
-
-
+    res.send(result)
+})
 
 // Change the gameState
 
 
 // name is like this becuase if quiz is in the name it does not work
 // dont ask me why it just is like that 
-app.get('/api/game/change_gameState', async (req, res) => {
+app.get('/api/game/pause_gameState', async (req, res) => {
     // var result = await knex('user');
     
-    kek = 
-    gameState = !gameState
+    kek = -8  
+    gameState = 3
     res.send('ok');
 })
 
-// Cheks if the border has passed the user 
-app.put('/api/game/catchedbyborder', async (req, res) => {
-    let users = await knex('user')
+app.get('/api/game/start_quiz', async (req, res) => {
+    // var result = await knex('user');
+    
+    kek = -8 
+    gameState = 2
+    res.send('quiz is starting');
+})
+
+
+
+
+// returns the game state
+
+app.get('/api/game/get_gameState', async (req, res) => {
+    // var result = await knex('user');
+    
+    res.send([gameState]);
+})
+
+
+// jsut kinda here for testing 
+async function fff() {
+    let user = knex('user').select('*')
+
+    let result = await knex('user')
         .join('user_answer', 'user.id', 'user_answer.user_id')
         .join('answer', 'user_answer.answer_id', 'answer.id')
         .select('user.nick')
-        .where('is_active', 1)
+        .where('is_correct', 1)
+        .where('user.is_active', 1)
         .count('answer.is_correct as correct')
         .groupBy('user.nick')
         .orderBy('correct', 'desc')
 
 
-    if (user.length === 0) {
+        console.log(result);
+        
+
+
+    // res.send(result)
+        
+}
+// fff()
+
+
+
+// Cheks if the border has passed the user 
+app.get('/api/game/catchedbyborder', async (req, res) => {
+    let users = await knex('user')
+        .join('user_answer', 'user.id', 'user_answer.user_id')
+        .join('answer', 'user_answer.answer_id', 'answer.id')
+        .select('user.nick')
+        .where('is_correct', 1)
+        .where('user.is_active', 1)
+        .count('answer.is_correct as correct')
+        .groupBy('user.nick')
+        .orderBy('correct', 'desc')
+        .select('user.id')
+
+    // console.log(users);
+    
+        
+
+    if (users.length === 0) {
         res.status(404)
         res.send('NOT FOUND\n')
     } else {
 
         // hier ist ein problem
         for (user of users) {
+            
             // Set the user if passed to inactive
-            if (kek > user.correct) {
+
+            var max = await knex('question').where('quiz_id','1')
+            
+            if (kek > user.correct * (100/ max.length)) {
+
+
                 // kek ist eine zahl von 0 - 100 und correct ist anzahl richtige fragen
                 //  :::TODO:::
                 // user.correct zu einer zahl von 1 - 100 convertieren um dann mit kek zu vergleichen.
@@ -323,7 +456,8 @@ app.put('/api/game/catchedbyborder', async (req, res) => {
             }
         }
     }
-
+    // yea its kinda pointless but hey not everything is vital
+    var result = await knex('user')
     res.send(result)
 })
 
@@ -332,11 +466,12 @@ app.put('/api/game/catchedbyborder', async (req, res) => {
 // Counter
 var lol = setInterval(() => {
 
-    if(gameState) {
+    if(gameState == 2) {
 
         if (kek >= 100) {
-            gameState = false
-            kek = 100
+            gameState = 1
+            kek = -8
+            change_isactive()
         }
 
         kek += 1
